46822eda87e804fada57a8df3f0fda375bf3f8bf
xwang345 <ryan.wang23@hotmail.com>
2024-05-05 22:21:09 -0400

    host cypress mochawesome report on localhost:3000

---
+++ mochawesome-reporter-server.ts
@@ -0,0 +1,15 @@
+import * as express from 'express';
+import * as path from 'path';
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, '.')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './cypress/reports/html/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
